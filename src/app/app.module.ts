83aa6bfcbd748c6490cdefc07d65c81c2b6cc899
Hackathonline.app <hackathonline.app@gmail.com>
2020-01-18 19:23:39 -0300

    material design setup and base module components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ReposListComponent } from './modules/repos-list/repos-list.component';
+import { DetailsComponent } from './modules/details/details.component';
+import { GridDetailsComponent } from './modules/grid-details/grid-details.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReposListComponent,
+    DetailsComponent,
+    GridDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
