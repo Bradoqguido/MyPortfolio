09bb262600fe8e4dfcf0d57b12e059c662b1b0a2
Bradoqguido <bradoqguido@gmail.com>
2020-02-29 22:57:03 -0300

    New route to path /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { DetailsComponent } from './components/details/details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
